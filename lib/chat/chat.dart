2283498901eff72fc0e55cce0a32e4700fd9ea50
Admin <admin@example.com>
2022-12-14 12:03:40 +0530

    Updated chat listing.

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -9,14 +9,31 @@ class ChatWidget extends StatelessWidget {
         itemCount: 10,
         itemBuilder: ((context, index) {
           return Column(
-            children: const [
+            children: [
+              const Divider(
+                color: Colors.black45,
+              ),
               ListTile(
-                leading: CircleAvatar(
+                leading: const CircleAvatar(
                   backgroundImage:
                       NetworkImage("https://picsum.photos/id/237/200/300"),
                 ),
-                title: Text("Praveen"),
-                subtitle: Text("This is the last message"),
+                title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Praveen",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "now",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ]),
+                subtitle: const Text(
+                  "This is the last message",
+                  style: TextStyle(fontSize: 16),
+                ),
               )
             ],
           );
